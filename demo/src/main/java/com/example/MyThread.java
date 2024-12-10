40017280ddbc35830ca46d56da5053d28a25f079
lai2-24 <lai2-24@itismeucci.com>
2024-12-10 14:40:42 +0100

    second commit

--- demo/src/main/java/com/example/MyThread.java
+++ demo/src/main/java/com/example/MyThread.java
@@ -2,9 +2,12 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 public class MyThread extends Thread {
 
@@ -19,28 +22,53 @@ public class MyThread extends Thread {
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
+            String firstLine = in.readLine();
             String s1 = "";
+            String[] request = firstLine.split(" ");
+
+            String method = request[0];
+            String resource = request[1];
+            String version = request[2];
+
             do {
                 s1 = in.readLine();
                 System.out.println(s1);
 
-                // se s1 È riga vuota significa che la richiesta È terminata
-                // quindi inizio a rispondere
-                // per ora si risponde sempre con 404
-                // farÒ una writebytes cosi:
+            } while (!s1.isEmpty());
 
-                // HTTP/1.1 404 Not found
-                // Content-Length: 0
-                // riga vuota
+            if (resource.equals("/")) {
+                resource = "/index.html";
+            }
 
-            } while (!s1.equals(""));
-            out.writeBytes("HTTP/1.1 404 Not found\r\n" +
-                                    "Content-Length: 0\r\n"
-                                    + "\r\n");
+            File file = new File("httdocs" + resource);
+            if (file.exists()) {
+
+                out.writeBytes("HTTP/1.1 200 ok\n");
+                out.writeBytes("Content-Type: text/html\n");
+                out.writeBytes("Content-Length:" + file.length() + " \n");
+                InputStream input = new FileInputStream(file);
+
+                byte[] buf = new byte[8192];
+                int n;
+                while ((n = input.read(buf)) != -1) {
+                    out.write(buf, 0, n);
+                }
+                input.close();
+
+            } else {
+                String resopnseBody = "not found ";
+                out.writeBytes("HTTP/1.1 404 not found\n");
+                out.writeBytes("Content-Type: text/HTML\n");
+                out.writeBytes("Content-Length:" + resopnseBody.length() + " \n");
+                out.writeBytes("\n");
+                out.writeBytes(resopnseBody);
+            }
+            s.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
 }
